70f0c4eb458ccd0e3dbf42efa7d5917fc0d3cdc3
ocengrave <ssaxalin@gmail.com>
2020-11-25 17:05:46 +0300

    Font style, new footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,11 @@
 import s_footer from './footer.module.css';
+import footer from './footer.png'
 const Footer=()=>
 {
     return(
-    <div className={s_footer.footer}>
         
+    <div className={s_footer.footer}>
+         
             <div className={s_footer.footer_info}>
                     <ul className={s_footer.footer_info_language_ul}>
                         <li>Русский</li>
@@ -13,9 +15,9 @@ const Footer=()=>
                         <li>Український</li>
                         <li>Ceština</li>
                     </ul>
+                    
             </div>
             <div className={s_footer.coppyright}>©Alexey Lyovin 2020</div>
-            
     </div>
     );
 }
