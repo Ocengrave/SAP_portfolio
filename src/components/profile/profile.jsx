39d219344d09fbcbbadf9995ba685fd1c59884b4
Ocengrave <ssaxalin@gmail.com>
2020-11-27 01:16:00 +0300

    Mobiles optimized prototype profile page

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,6 +2,7 @@ import s_profile from './profile.module.css';
 import s_profile_settings from './profile_settings.module.css';
 function Profile() {
     return (
+      <div>
       <div className={s_profile.profile}>
           <div className={s_profile.header}></div>
           <div className={s_profile.left_margin}></div>
@@ -17,8 +18,13 @@ function Profile() {
                        <li>Настройки</li>
                    </ul>
                     </div>
+                
             <div className={s_profile.about_profile}>
-                <div className={s_profile_settings.avatar}>Аватар</div>
+                <div className={s_profile_settings.avatar}>
+                  <div className={s_profile_settings.avatarimg}>
+                  <img src="https://sun9-26.userapi.com/impg/fY54ae27V-Vcy4XXJhS2t10ps5BQk0I4QsNGQA/Xol7_0_s7gg.jpg?size=1622x2160&quality=96&proxy=1&sign=2de3a4e88430bbf057923a776f642f25" alt="" srcset=""/>
+                  </div>
+                  </div>
                 <div className={s_profile_settings.about_profile_info}>Информация о профиле</div>
                 <div className={s_profile_settings.friends}><div className={s_profile_settings.list_friend}>Список друзей</div>
                 
@@ -44,9 +50,34 @@ function Profile() {
                 <div className={s_profile_settings.info}>Информационная лента</div>
                 </div>
             </div>
+
             <div className={s_profile.right_margin}></div>
       </div>
-       
+       <div className={s_profile.profile_mobile}>
+         <div className={s_profile.header_mobile}>
+         <div className={s_profile.menu_mobile}>
+         <div className={s_profile.menu_mobile_item}>Главная</div>
+             <div className={s_profile.menu_mobile_item}>Новости</div>
+             <div className={s_profile.menu_mobile_item}>Музыка</div>
+             <div className={s_profile.menu_mobile_item}>Друзья</div>
+             <div className={s_profile.menu_mobile_item}>Видео</div>
+             <div className={s_profile.menu_mobile_item}>Настройки</div>
+           </div>
+         </div>
+         <div className={s_profile.center_wrapper_mobile}>
+         <div className={s_profile.menu_mobile_small}>
+             <div className={s_profile.menu_mobile_item}>Главная</div>
+             <div className={s_profile.menu_mobile_item}>Новости</div>
+             <div className={s_profile.menu_mobile_item}>Музыка</div>
+             <div className={s_profile.menu_mobile_item}>Друзья</div>
+             <div className={s_profile.menu_mobile_item}>Видео</div>
+             <div className={s_profile.menu_mobile_item}>Настройки</div>
+           </div>
+         <div className={s_profile.avatar_mobile}>аватар</div>
+         <div className={s_profile.profil_info_mobile}>инфо</div>
+           container</div>
+       </div>
+       </div>
     );
   }
   
